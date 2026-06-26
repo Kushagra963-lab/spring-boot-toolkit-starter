4e3eb1dbff8d5133602c8ea96acbb66290b1543d
Kushagra Singh Gaur <kushagrasingh.gaur2022@vitstudent.ac.in>
2026-06-26 10:20:33 +0530

    Add ToolkitConstants.java

---
+++ toolkit-core/src/main/java/io/github/kushagrasinghga/toolkit/constants/ToolkitConstants.java
@@ -0,0 +1,9 @@
+package io.github.kushagrasinghga.toolkit.constants;
+
+public final class ToolkitConstants {
+    public static final String CORRELATION_ID_HEADER = "X-Correlation-ID";
+    public static final String CORRELATION_ID_MDC_KEY = "correlationId";
+
+    private ToolkitConstants() {
+    }
+}
