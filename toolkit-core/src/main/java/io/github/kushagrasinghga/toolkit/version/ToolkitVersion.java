742776e8fbbd245a2295da15bbe0e79c2e2c6adf
Kushagra Singh Gaur <kushagrasingh.gaur2022@vitstudent.ac.in>
2026-06-26 10:24:52 +0530

    Add ToolkitVersion.java

---
+++ toolkit-core/src/main/java/io/github/kushagrasinghga/toolkit/version/ToolkitVersion.java
@@ -0,0 +1,8 @@
+package io.github.kushagrasinghga.toolkit.version;
+
+public final class ToolkitVersion {
+    public static final String VERSION = "1.0.0";
+
+    private ToolkitVersion() {
+    }
+}
